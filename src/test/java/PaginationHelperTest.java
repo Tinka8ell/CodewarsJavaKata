14bf49cec3fbd669f5d6dc44682a4211290179ea
Mark Ingamells <mark.ingamells@gmail.com>
2022-03-23 17:40:51 +0000

    Pagination Helper Kata

---
+++ src/test/java/PaginationHelperTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaginationHelperTest {
+    /* Given samples:
+        PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+        helper.pageCount(); //should == 2
+        helper.itemCount(); //should == 6
+        helper.pageItemCount(0); //should == 4
+        helper.pageItemCount(1); // last page - should == 2
+        helper.pageItemCount(2); // should == -1 since the page is invalid
+
+        // pageIndex takes an item index and returns the page that it belongs on
+        helper.pageIndex(5); //should == 1 (zero based index)
+        helper.pageIndex(2); //should == 0
+        helper.pageIndex(20); //should == -1
+        helper.pageIndex(-10); //should == -1
+     */
+    @Test
+    public void checkExample(){
+        PaginationHelper<Character> helper =
+                new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+        assertEquals(2, helper.pageCount(), "pageCount()");
+        assertEquals(6, helper.itemCount(), "itemCount()");
+        assertEquals(4, helper.pageItemCount(0), "pageItemCount(0)");
+        assertEquals(2, helper.pageItemCount(1), "pageItemCount(1)");
+        assertEquals(-1, helper.pageItemCount(2), "pageItemCount(2)");
+
+        assertEquals(1, helper.pageIndex(5), "pageIndex(5)");
+        assertEquals(0, helper.pageIndex(2), "pageIndex(2)");
+        assertEquals(-1, helper.pageIndex(20), "pageIndex(20)");
+        assertEquals(-1, helper.pageIndex(-10), "pageIndex(-10)");
+    }
+
+}
