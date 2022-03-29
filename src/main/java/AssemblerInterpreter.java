d176a687efc7fdc7902c8e2ebc08664b1e032304
Mark Ingamells <mark.ingamells@gmail.com>
2022-03-29 20:17:58 +0100

    Continuing the Assembler Interpreter  Kata Yes, I know I should commit more often! Completed the "assemble" part and structure of the "virtual machine" ready to write the interpreting of instructions.

--- src/main/java/AssemblerInterpreter.java
+++ src/main/java/AssemblerInterpreter.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * This is the second part of this kata series. First part is here.
  *
@@ -76,16 +79,124 @@
  *    prepares the output of the program
  *    and then returns it with the end instruction.
  * In this case, the output would be (5+1)/2 = 3.
+ *
+ * Based on the previous Kata (SimpleAssembler) we will need to identify
+ * the important parts of the language,
+ * translate the source into "machine code" based on the analysis above
+ * then run it in a virtual machine.
+ *
+ * Assembly:
+ * current location = 0
+ * each line:
+ *    remove anything after (and including) a ';' - comments
+ *    strip leading ad trailing spaces
+ *    if none blank:
+ *       split at first space: op-code and (trimmed) rest
+ *       if op-code ends in ':' treat as label:
+ *          add label (less ':') with current location to look-up map
+ *       else
+ *          store validates opcode (against valid / known ones) in current location
+ *          parse rest according to format for op-code:
+ *             [[<left-hand>, ] <right-hand>] - assigned to and assignee
+ *             <right-hand>, <right-hand> - two read from values to be compared
+ *             <string>|<register> [, <string>|<register>]*
+ *             where:
+ *                <left-hand> is a <register>
+ *                <right-hand> is <register>|<label>|<integer>
+ *                <string> is "'" <characters>* "'"
+ *                <register> is <letter>
+ *          store code as String[] - <op-code> [<parameter>]*
+ *          increment current location
+ * machine code:
+ *    code - List of String[]
+ *    labels - Map of String, Integer
+ *
+ * Virtual machine:
+ *    pointer - int of position in code
+ *    stack - may not need to be stack, of return pointers
+ *    registers - Map of String, Integer
+ *    output - String of appended output (or null)
  */
 public class AssemblerInterpreter {
 
+    private static List<List<String>> code;
+    private static Map<String, Integer> labels;
+
     public static String interpret(final String input) {
         // Convert input into "machine code"
-
+        assemble(input);
         // Run the code
         String result = null;
+        int pointer = 0;
+        boolean finished = false;
+        while (!finished){
+            // run the code
+            // get the instruction and move pointer to 'next'
+            List<String> instruction = code.get(pointer++);
+            String opCode = instruction.get(0);
+            // action opcode
+            switch (opCode){
 
+            }
+        }
         // Output the fine result
         return result;
+    }
+
+    private static void assemble(final String input) {
+        List<String> lines = Arrays.stream(input.split("\n"))
+                .map(s -> {
+                    int pos = s.indexOf(';');
+                    return (pos != -1) ? s.substring(0, pos) : s;
+                })
+                .map(String::strip)
+                .filter(str -> str.trim().length() > 0)
+                .collect(Collectors.toList());
+        code = new ArrayList<>();
+        labels = new HashMap<>();
+        for (String line: lines) {
+            int pos = line.indexOf(' ');
+            String opCode = line;
+            List<String> parameters = new ArrayList<>();
+            if (pos == -1) {
+                if (opCode.endsWith(":")){
+                    // add a label where the next element of code will be
+                    labels.put(opCode.substring(0, opCode.length()-1), code.size());
+                    opCode = null;
+                }
+            }
+            else {
+                opCode = line.substring(0, pos);
+                line =  line.substring(pos).trim();
+                pos = line.indexOf('\'');
+                if (pos != -1){
+                    // have at least one string so must be message format
+                    // must check is "'" is first ig this is still true ....
+                    // split into strings (odd elements) and non-strings (even elements)
+                    String[] alternates = line.split("'");
+                    for (int i = 0; i < alternates.length; i++) {
+                        if (i % 2 == 0){
+                            // even - non-strings
+                            parameters.addAll(Arrays.stream(line.split(","))
+                                    .map(String::trim)
+                                    .collect(Collectors.toList()));
+                        }
+                        else {
+                            //odd - string
+                            parameters.add(alternates[i]);
+                        }
+                    }
+                } else {
+                    // only csv!
+                    parameters = Arrays.stream(line.split(","))
+                            .map(String::trim)
+                            .collect(Collectors.toList());
+                }
+            }
+            if (opCode != null) {// add as next element
+                parameters.add(0, opCode);
+                code.add(parameters);
+            }
+        }
     }
 }
