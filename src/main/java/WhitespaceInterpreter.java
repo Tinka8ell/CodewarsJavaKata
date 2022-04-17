8801548c58fda09dc1def3015b24347db0f8bbc4
Mark Ingamells <mark.ingamells@gmail.com>
2022-04-17 14:38:41 +0100

    Added converter to how I want to see the code - Whitespace Interpreter Kata

--- src/main/java/WhitespaceInterpreter.java
+++ src/main/java/WhitespaceInterpreter.java
@@ -1,7 +1,6 @@
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class WhitespaceInterpreter {
 
@@ -10,13 +9,35 @@ public class WhitespaceInterpreter {
         return code != null ? code.replace(' ', 's').replace('\t', 't').replace('\n', 'n') : null;
     }
 
+    /**
+     * Convert the "whitespace" source code into a queue of tri-state codes.
+     * As:
+     *    ' ' is often the equivalent of binary 0, so use 0
+     *    '\t' is often the equivalent of binary 1, so use 1
+     *    '\n' if usually more special, so use -1
+     *
+     * @param source String of Whitespace code
+     * @return a Queue of Integer codes
+     */
+    public static Queue<Integer> toCodes(String source){
+        return source
+                .chars()
+                .filter(Character::isWhitespace)
+                .map(c -> (c == ' ') ? 0 : (c == '\t') ? 1 : -1)
+                .boxed().collect(Collectors.toCollection(ArrayDeque::new));
+    }
+
     // solution
     public static String execute(String code, InputStream input) {
-        String output = "";
+        StringBuilder output = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
         Map<Integer,Integer> heap = new HashMap<>();
-        // ... you code ...
-        return output;
+        Queue<Integer> codeQueue = toCodes(code);
+        while (codeQueue.peek() != null) {
+            int sign = codeQueue.poll();
+            output.append((sign == 0) ? '0' : (sign > 0) ? '+' : '-');
+        }
+        return output.toString();
     }
 
 }
