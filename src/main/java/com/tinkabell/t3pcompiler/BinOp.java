9a1ba80aeda489500068bfb4eff27746cfadb110
Tinka8ell <mark.ingamells@gmail.com>
2022-09-16 16:05:57 +0100

    Continued set up for new 1kyu Java Kata Better understanding of the Java framework, but tests don't work even though Java framework is complete

--- src/main/java/com/tinkabell/t3pcompiler/BinOp.java
+++ src/main/java/com/tinkabell/t3pcompiler/BinOp.java
@@ -2,6 +2,10 @@ package com.tinkabell.t3pcompiler;
 
 public class BinOp implements Ast {
 
+    private final String command;
+    private final Ast left;
+    private final Ast right;
+
     /**
      * new BinOp('+', a, b)       // add subtree a to subtree b
      * new BinOp('-', a, b)       // subtract subtree b from subtree a
@@ -9,17 +13,28 @@ public class BinOp implements Ast {
      * new BinOp('/', a, b)       // divide subtree a from subtree b
      */
     public BinOp(String  op, Ast a, Ast b) {
+        command = op;
+        left = a;
+        right = b;
     }
 
     public Ast a(){
-        return null;
+        return left;
     }
     public Ast b(){
-        return null;
+        return right;
     }
 
     @Override
     public String op() {
-        return null;
+        return command;
+    }
+
+    /*
+     * {'op':'?','a':?,'b':?}
+     */
+    @Override
+    public String toString() {
+        return "{'op':'" + command + "','a':" + left + ",'b':" + right + "}";
     }
 }
--- src/main/java/com/tinkabell/t3pcompiler/UnOp.java
+++ src/main/java/com/tinkabell/t3pcompiler/UnOp.java
@@ -2,19 +2,30 @@ package com.tinkabell.t3pcompiler;
 
 public class UnOp implements Ast{
 
+    private final String command;
+    private final int value;
+
+
     /**
      * new UnOp('arg', n)         // reference to n-th argument, n integer
      * new UnOp('imm', n)         // immediate value n, n integer
      */
     public UnOp(String command, int n) {
+        this.command = command;
+        value = n;
     }
 
     public int n() {
-        return 0;
+        return value;
     }
 
     @Override
     public String op() {
-        return null;
+        return this.command;
+    }
+
+    @Override
+    public String toString() {
+        return "{'op':'" + command + "','n':" + value + "}";
     }
 }
